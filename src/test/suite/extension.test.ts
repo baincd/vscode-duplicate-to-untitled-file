7be3582e60c95aadab7611bf8d69d06d9a75b557
Chris Bain <baincd@users.noreply.github.com>
2021-04-24 15:26:03 -0400

    Convert test scaffolding to chai, TDD style

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,12 @@
-import * as assert from 'assert';
+import {expect} from 'chai'
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
+describe('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	it('Sample test', async () => {
+		expect([1, 2, 3].indexOf(5)).to.be.eq(-1)
+		expect([1, 2, 3].indexOf(1)).to.be.eq(-1)
 	});
 });
