83092c9c4d6d956d0758b264cc76844544b43299
Chris Bain <baincd@users.noreply.github.com>
2021-04-25 09:27:15 -0400

    Add "Replace with Duplicated..." Command

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,35 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	async function duplicateToNewUntitledFile() {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('duplicate-to-untitled-file.duplicateToNewUntitledFile', () => {
+			duplicateToNewUntitledFile(false);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('duplicate-to-untitled-file.replaceWithDuplicatedNewUntitledFile', () => {
+			duplicateToNewUntitledFile(true);
+		})
+	);
+
+	async function duplicateToNewUntitledFile(replace: boolean) {
 		if (!vscode.window.activeTextEditor?.document) {
 			vscode.window.showErrorMessage("Unable to duplicate non text file");
 			return;
 		}
+		let srcDoc = vscode.window.activeTextEditor.document;
 		const newDoc = await vscode.workspace.openTextDocument( {
 			language: vscode.window.activeTextEditor?.document.languageId,
 			content: vscode.window.activeTextEditor?.document.getText()
 		});
-		await vscode.window.showTextDocument(newDoc);
+		await vscode.window.showTextDocument(newDoc, {preview: false});
+		if (replace) {
+			await vscode.window.showTextDocument(srcDoc, {preview: false});
+			await vscode.commands.executeCommand("workbench.action.closeActiveEditor");			
+		}
 	}
 
-	let disposable = vscode.commands.registerCommand('duplicate-to-untitled-file.duplicateToNewUntitledFile', () => {
-		duplicateToNewUntitledFile();
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
